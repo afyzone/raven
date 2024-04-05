690108d790632e9245fe2a2ef26f7c1175a4fb4b
afy <73753274+afyzone@users.noreply.github.com>
2024-04-04 23:53:14 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -283,5 +283,7 @@ getgenv().settings = {
     }
 }
 
-script_key = settings.script_key
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9e3f24cc781e50a903bcbae90a4cf1f0.lua"))()
+if (not getgenv().ravenhook_executed) then
+    script_key = settings.script_key
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9e3f24cc781e50a903bcbae90a4cf1f0.lua"))()
+end
