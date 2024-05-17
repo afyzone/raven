040a827d01b7a444cc5cff61ca95c5b9eb9d98b4
afy <73753274+afyzone@users.noreply.github.com>
2024-05-16 19:51:50 -0700

    Update endpoint.lua

--- endpoint.lua
+++ endpoint.lua
@@ -1,4 +1,4 @@
 if (not getgenv().ravenhook_executed) then
-    script_key = getgenv().settings.script_key
+    getgenv().script_key = getgenv().settings.script_key
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8a393de97072eb46055c5b1fc6ea6d54.lua"))()
 end
