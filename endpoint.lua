456bb335683f263421b50632dc0eadd883458b60
afy <73753274+afyzone@users.noreply.github.com>
2025-02-22 03:06:27 -0800

    Delete endpoint.lua

--- endpoint.lua
+++
@@ -1,4 +0,0 @@
-if (not getgenv().ravenhook_executed) then
-    getgenv().script_key = getgenv().settings.script_key
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8a393de97072eb46055c5b1fc6ea6d54.lua"))()
-end
